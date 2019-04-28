97b4222d4a45e4ffc4097c658c622a91c2fe9c35
semelyanov86 <sergey86@gmail.com>
2019-04-28 10:56:50 +0300

    Rewrote saveRecord function in PBXManager module

--- modules/PBXManager/models/Record.php
+++ modules/PBXManager/models/Record.php
@@ -415,5 +415,102 @@ class PBXManager_Record_Model extends Vtiger_Record_Model{
         }
         return $numbers;
     }
+
+    /**
+     * Function to save the current Record Model. Function rewrote by Sergey Emelyanov. It deletes previous PBXRecord to prevent dublicates
+     */
+    public function save() {
+        global $adb;
+        $status = $this->get('callstatus');
+        $sourceid = $this->get('sourceuuid');
+        $number = $this->get('customernumber');
+        if (true) {
+            $query="select vtiger_pbxmanager.pbxmanagerid, vtiger_pbxmanagercf.cf_1250 from vtiger_pbxmanager inner join vtiger_crmentity on vtiger_crmentity.crmid=vtiger_pbxmanager.pbxmanagerid inner join vtiger_pbxmanagercf on vtiger_pbxmanagercf.pbxmanagerid=vtiger_pbxmanager.pbxmanagerid where vtiger_crmentity.deleted=0 and  vtiger_pbxmanager.sourceuuid = ?";
+            $result = $adb->pquery($query, array($sourceid));
+            if($adb->num_rows($result) >= 1){
+                while($result_set = $adb->fetch_array($result))
+                {
+                    $previd = $result_set["pbxmanagerid"];
+                    $leadid = false;
+                    $prevModel = PBXManager_Record_Model::getInstanceById($previd);
+                    if ($prevModel->get('callstatus') === 'no-answer') {
+                        $leadid = $result_set["cf_1250"];
+                        $prevModel->delete();
+                    } elseif ($prevModel->get('callstatus') === 'completed') {
+                        return false;
+                    }
+                    if ($leadid){
+                        $this->deleteLeadById($leadid);
+                    }
+                }
+            } else {
+                $query = "SELECT vtiger_pbxmanager.pbxmanagerid, vtiger_pbxmanager.customernumber, vtiger_pbxmanagercf.cf_1250, vtiger_pbxmanager.callstatus FROM vtiger_pbxmanager INNER JOIN vtiger_crmentity on vtiger_pbxmanager.pbxmanagerid=vtiger_crmentity.crmid INNER JOIN vtiger_pbxmanagercf ON vtiger_pbxmanagercf.pbxmanagerid = vtiger_pbxmanager.pbxmanagerid ORDER BY vtiger_pbxmanager.pbxmanagerid DESC LIMIT 3";
+                $result = $adb->pquery($query, array());
+                if($adb->num_rows($result) >= 1){
+                    while($result_set = $adb->fetch_array($result))
+                    {
+                        $previd = $result_set["pbxmanagerid"];
+                        $prevNumber = $result_set['customernumber'];
+                        $status = $result_set['callstatus'];
+                        $leadid = false;
+                        if ($status === 'no-answer' && $prevNumber === $number) {
+                            $prevModel = PBXManager_Record_Model::getInstanceById($previd);
+                            $leadid = $result_set["cf_1250"];
+                            $prevModel->delete();
+                        } elseif ($status === 'completed' && $prevNumber === $number) {
+                            return false;
+                        }
+                        if ($leadid){
+                            $this->deleteLeadById($leadid);
+                        }
+                    }
+                }
+            }
+            $noAnswerLead = $this->createLeadRecord();
+            $this->getModule()->saveRecord($this);
+        } else {
+            $this->getModule()->saveRecord($this);
+        }
+    }
+
+    /*
+     * Function created Lead Record if current call status is 'No Answer.
+     * return Lead model
+     */
+    public function createLeadRecord()
+    {
+        $number = $this->get('customernumber');
+        $leadModel = Vtiger_Record_Model::getCleanInstance('Leads');
+        $leadModel->set('mode', 'create');
+        $leadModel->set('lastname', 'Missed Call');
+        $leadModel->set('phone', $number);
+        $leadModel->set('description', $this->get('sourceuuid'));
+        $leadModel->save();
+        $this->set('cf_1250', $leadModel->getId());
+        if (!$this->get('customer')) {
+            $this->set('customer', $leadModel->getId());
+        }
+        return $leadModel;
+
+    }
+    
+    /*
+     * Function deletes Lead Record by id
+     * @param int Lead id
+     * return boolean depend on successful implementation.
+     */
+    private function deleteLeadById($leadid)
+    {
+        if (!$leadid) {
+            return false;
+        }
+        $leadModel = Vtiger_Record_Model::getInstanceById($leadid, 'Leads');
+        if ($leadModel) {
+            $leadModel->delete();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 ?>
